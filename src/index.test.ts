1328517b9fb73652af827ac0fd83a25d1aa2bc32
Melanie Seltzer <melleh11@gmail.com>
2019-08-22 10:02:04 -0700

    test: add basic test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,12 @@
+import { createRef } from 'react';
+import { renderHook } from '@testing-library/react-hooks';
 import useCaretPosition from '.';
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(useCaretPosition).toBeTruthy();
+describe('useCaretPosition', () => {
+  it('should initialize positions to null', () => {
+    const someRef = createRef<HTMLFormElement>();
+    const { result } = renderHook(() => useCaretPosition(someRef));
+    expect(result.current.start).toBe(null);
+    expect(result.current.end).toBe(null);
   });
 });
