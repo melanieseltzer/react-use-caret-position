df159a65c1d06535f7941f7be73aea9a184c1a89
Melanie Seltzer <melleh11@gmail.com>
2019-08-22 00:27:38 -0700

    feat: add types

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { useState } from 'react';
+import { useState, RefObject } from 'react';
 
-function useCaretPosition(node: any) {
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(0);
+function useCaretPosition(node: RefObject<HTMLFormElement>) {
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
 
   const updateCaret = () => {
     // Get the updated caret postions from the ref passed in
-    const { selectionStart } = node.current;
-    const { selectionEnd } = node.current;
+    if (node && node.current) {
+      const { selectionStart } = node.current;
+      const { selectionEnd } = node.current;
 
-    // Update the state
-    setStart(selectionStart);
-    setEnd(selectionEnd);
+      // Update the state
+      setStart(selectionStart);
+      setEnd(selectionEnd);
+    }
   };
 
   const setCaretPosition = () => {
     // Set the caret position by setting the selection range with the
     // most current start and end values
-    node.current.setSelectionRange(start, end);
+    if (node && node.current && start && end) {
+      node.current.setSelectionRange(start, end);
+    }
   };
 
   return { start, end, updateCaret, setCaretPosition };
