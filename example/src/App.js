c3d26845f02509acf9ab8bcb1b614b87d6e5e713
Melanie Seltzer <melleh11@gmail.com>
2019-08-22 20:39:07 -0700

    feat: add better example

--- example/src/App.js
+++ example/src/App.js
@@ -1,47 +1,14 @@
-import React, { useEffect, useState, useRef } from 'react';
-import useCaretPosition from 'react-use-caret-position';
+import React from 'react';
 
-const Input = () => {
-  // Some sort of input state
-  const [text, setText] = useState('hello');
+import InputWithHook from './components/InputWithHook';
+import InputWithoutHook from './components/InputWithoutHook';
+import Layout from './components/Layout';
 
-  // Must create a ref
-  const inputRef = useRef();
-
-  // Pass the ref into the hook
-  const { start, end, updateCaret, setCaretPosition } = useCaretPosition(
-    inputRef
-  );
-
-  useEffect(() => {
-    console.log(`selectionStart: ${start}`);
-    console.log(`selectionEnd: ${end}`);
-    // Set the caret position on mount + update
-    setCaretPosition();
-  });
-
-  const handleChange = e => {
-    // Some sort of side effect in the onChange handler
-    // which normally makes the input lose its position
-    const inputToUpperCase = e.target.value.toUpperCase();
-    setText(inputToUpperCase);
-
-    // `updateCaret` will update the selectionStart and selectionEnd
-    // values of the input
-    updateCaret();
-  };
-
-  return (
-    <input
-      type="text"
-      ref={inputRef}
-      value={text}
-      onChange={e => handleChange(e)}
-      style={{ fontSize: '2rem' }}
-    />
-  );
-};
-
-const App = () => <Input />;
+const App = () => (
+  <Layout>
+    <InputWithHook />
+    <InputWithoutHook />
+  </Layout>
+);
 
 export default App;
---
+++ example/src/components/InputWithHook.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState, useRef } from 'react';
+import useCaretPosition from 'react-use-caret-position';
+import Input from './Input';
+
+const highlight = {
+  background: '#ffcccc',
+  padding: '2px',
+  fontWeight: '400'
+};
+
+const InputWithRef = () => {
+  // Some sort of input state
+  const [text, setText] = useState('hello world');
+
+  // Must create a ref
+  const inputRef = useRef();
+
+  // Pass the ref into the hook
+  const { start, end, updateCaret, setCaretPosition } = useCaretPosition(
+    inputRef
+  );
+
+  useEffect(() => {
+    // Set the caret position on mount + update
+    setCaretPosition();
+  });
+
+  const handleChange = e => {
+    // Some sort of side effect in the onChange handler
+    // which normally makes the input lose its position
+    const inputToUpperCase = e.target.value.toUpperCase();
+    setText(inputToUpperCase);
+
+    // `updateCaret` will update the selectionStart and selectionEnd
+    // values of the input
+    updateCaret();
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: '700px',
+        background: '#eee',
+        padding: '0 1rem',
+        marginBottom: '1rem'
+      }}
+    >
+      <h2 style={{ fontWeight: '400' }}>Input with useCaretPosition</h2>
+      <p>
+        start: {start}, end: {end}
+      </p>
+      <Input
+        label="with"
+        ref={inputRef}
+        value={text}
+        onChange={e => handleChange(e)}
+      />
+      <p style={{ lineHeight: '24px' }}>
+        <mark style={highlight}>useCaretPosition</mark> cleverly keeps track of
+        the input <mark style={highlight}>selectionStart</mark> and{' '}
+        <mark style={highlight}>selectionEnd</mark>, so we no longer get booted
+        to the end of the input... nice!{' '}
+        <span role="img" aria-label="sunglasses emoji">
+          😎
+        </span>
+        <span role="img" aria-label="thumbs up emoji">
+          👍
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default InputWithRef;
